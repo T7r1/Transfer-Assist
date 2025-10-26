c813b14a3efbdbf3c8254e67acc0f46846a36e15
T7r1 <nickgin1207@gmail.com>
2025-10-26 00:42:39 -0700

    Nothing of value

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("input");
 const ls = document.getElementById("myList");
+myColleges = [];
 
+//college search and add-er
 btn.addEventListener("click", () => {
   const value = input.value.trim();
   if (value) {
     const li = document.createElement("li");
     li.textContent = value;
     ls.appendChild(li);
+
+    //updates college list
+    myColleges.push(value);
     input.value = "";
   } else {
     alert("Please enter a list item.");
   }
 });
-// const addBtn = document.getElementById("addBtn");
-// const newItem = document.getElementById("newItem");
-// const list = document.getElementById("myList");
 
-// addBtn.addEventListener("click", () => {
-//   const value = newItem.value.trim();
-//   if (value) {
-//     const li = document.createElement("li");
-//     li.textContent = value;
-//     list.appendChild(li);
-//     newItem.value = "";
-//   } else {
-//     alert("Please enter a list item.");
-//   }
-// });
+//reads university.json file
+fetch("university.json")
+  .then((response) => response.json()) // Automatically parses the JSON response
+  .then((data) => {
+    console.log(data);
+    const selectElement = document.getElementById("collegeSelect");
+
+    data.forEach((college) => {
+      const newOption = document.createElement("option");
+      newOption.textContent = college.name; // The text displayed to the user
+      newOption.value = college.name; // The value submitted with the form
+      selectElement.appendChild(newOption);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching university data:", error);
+  });
+// const collegeCheckList = document.getElementById("collegeCheckList");
+// for (let i = 0; i < array.length; i++) collegeCheckList.appendChild(college);
